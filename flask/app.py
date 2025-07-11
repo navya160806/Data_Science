6e2cfc7a4ec551e40130cf486235a09d9562b072
navya singh <iamnavyasingh2006@gmail.com>
2025-07-11 18:00:31 +0530

    library

--- flask/app.py
+++ flask/app.py
@@ -13,6 +13,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/register')
 def register():
     return render_template('register.html')
