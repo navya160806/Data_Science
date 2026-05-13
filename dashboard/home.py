6e1c56ab7a6fa644780fb6d67d2d91a1ac9658ba
navya singh <iamnavyasingh2006@gmail.com>
2026-05-13 23:37:09 +0530

    .

---
+++ dashboard/home.py
@@ -0,0 +1,7 @@
+#to run streamlit== streamlit run home.py
+import  streamlit as st
+
+st.markdown("<h1 style ='text-align: center; color:#FF6347;"
+            "border: 1px solid white '>Car Crashes Dashboard</h1>",unsafe_allow_html=True)
+
+
